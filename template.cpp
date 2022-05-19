6fae335b3ffa94a51d472b5ab5962c033d9413d8
Pietro Polinari Cavassin <pietro.polinari@gmail.com>
2022-05-19 17:33:22 -0300

    try 1391

--- template.cpp
+++ template.cpp
@@ -4,7 +4,7 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-#define MAX_LL 0x7fffffffffffffff
+#define LL_MAX 0x7fffffffffffffff
 
 #define MP make_pair
 #define PB push_back
