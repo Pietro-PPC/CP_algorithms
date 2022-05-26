f7aed322c5d43d6528d915c883209683821ca13c
Pietro Polinari Cavassin <pietro.polinari@gmail.com>
2022-05-26 18:18:08 -0300

    add muf

---
+++ data_strucs/muf.cpp
@@ -0,0 +1,15 @@
+int _p[], _rank[];
+
+void _make(int a){ _p[a] = a; _rank[a] = 1;}
+
+int _find(int a){ return _p[a] == a ? a : (_p[a] = _find(_p[a]) );}
+
+void _union(int a, int b){
+    a = _find(a); b = _find(b);
+    if (_rank[a] > _rank[b])
+        _p[b] = a;
+    else {
+        if (_rank[a] == _rank[b]) _rank[b]++;
+        _p[a] = b;
+    }
+}
