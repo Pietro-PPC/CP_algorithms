eb673e57b467f18f083cca2584c4e712bc382948
Pietro Polinari Cavassin <pietro.polinari@gmail.com>
2022-05-28 14:21:40 -0300

    [wip] uva10132

--- in_progress/uva10132.cpp
+++ in_progress/uva10132.cpp
@@ -16,19 +16,59 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int t; cin >> t;
+    int t; 
     char c;
     string buf;
-
-    cin.get(c);
+    map<string, bool> m;
+    
+    cin >> t; cin.get(c);
     getline(cin, buf);
     while (t--){
+        int min, max;
+        int cntini[2], cntfim[2];
         vector<vector<string>> v(MAX);
+
         getline(cin, buf);
+        memset(cntini, 0, sizeof(cntini));
+        memset(cntfim, 0, sizeof(cntfim));
+        min = max = buf.length();
         while (buf.length() > 0){
-            // cout << buf << " " << buf.length() << endl;
-            v[buf.length()].PB(buf);
+            if (m.find(buf) == m.end()){
+                v[buf.length()].PB(buf);
+                cntini[buf[0]]++;
+                cntfim[buf[buf.length()-1]]++;
+                m[buf] = 1;
+            }
+            if (buf.length() < min) min = buf.length();
+            if (buf.length() > max) max = buf.length();
             getline(cin, buf);
+        }
+        
+        int startswith = -1, endswith = -1;
+        if (cntini[0] > cntini[1]) startswith = 0;
+        else if (cntini[0] < cntini[1]) startswith = 1;
+        if (cntfim[0] > cntfim[1]) endswith = 0;
+        else if (cntfim[0] < cntfim[1]) endswith = 1;
+
+        int mid = (min+max)/2;
+        int str_size = min+max;
+        vector<string> answers;
+        for (int i = min; i <= mid; ++i){
+            cout << i << endl;
+            switch (v[i].size()){
+                case 1:
+                    answers.PB(v[i][0] + v[max-i][0]);
+                    answers.PB(v[max-i][0] + v[i][0]);
+                case 2: 
+                    answers.PB(v[i][0] + v[max-i][1]);
+                    answers.PB(v[i][1] + v[max-i][0]);
+                    answers.PB(v[i][1] + v[max-i][1]);
+                    answers.PB(v[max-i][0] + v[i][1]);
+                    answers.PB(v[max-i][1] + v[i][0]);
+                    answers.PB(v[max-i][1] + v[i][1]);
+                break;
+            }
+
         }
     }
 
