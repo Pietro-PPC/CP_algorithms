5d4ab5f071a45a9d69f8167c879b5e8cde074dff
Pietro Polinari Cavassin <pietro.polinari@gmail.com>
2022-06-06 22:05:01 -0300

    solve 1402 (more readable)

--- beecrowd/bee1402.cpp
+++ beecrowd/bee1402.cpp
@@ -12,10 +12,13 @@ typedef pair<ll, ll> pll;
 
 #define MAX 1123
 
-int xyl[MAX][3];
+typedef struct {
+    int x, y, l;
+} wall_t;
+
+wall_t walls[MAX];
 double g[MAX][MAX], dsts[MAX], pathsize[MAX];
 int vis[MAX];
-
 int n;
 
 void change (int &a, int &b){
@@ -24,52 +27,51 @@ void change (int &a, int &b){
 
 double calc_dist(int w1, int w2){
     double dx = 0.0, dy = 0.0;
-    if (xyl[w1][2] >= 0 && xyl[w2][2] >= 0){
-        if (xyl[w1][0] >= xyl[w2][0]) change(w1, w2);
-        dy = abs(xyl[w1][1] - xyl[w2][1]);
-        dx = max(0, xyl[w2][0] - (xyl[w1][0] + xyl[w1][2]));
+    if (walls[w1].l >= 0 && walls[w2].l >= 0){
+        if (walls[w1].x >= walls[w2].x) change(w1, w2);
+        dy = abs(walls[w1].y - walls[w2].y);
+        dx = max(0, walls[w2].x - (walls[w1].x + walls[w1].l));
     }
-    else if (xyl[w1][2] < 0 && xyl[w2][2] < 0){
-        if (xyl[w1][1] >= xyl[w2][1]) change(w1, w2);
-        dx = abs(xyl[w1][0] - xyl[w2][0]);
-        dy = max(0, xyl[w2][1] - (xyl[w1][1] + (-xyl[w1][2])));
+    else if (walls[w1].l < 0 && walls[w2].l < 0){
+        if (walls[w1].y >= walls[w2].y) change(w1, w2);
+        dx = abs(walls[w1].x - walls[w2].x);
+        dy = max(0, walls[w2].y - (walls[w1].y + (-walls[w1].l)));
     }
     else {
-        if (xyl[w1][2] < 0) change(w1, w2);
-        if (xyl[w1][0] >= xyl[w2][0])
-            dx = xyl[w1][0] - xyl[w2][0];
+        if (walls[w1].l < 0) change(w1, w2);
+        if (walls[w1].x >= walls[w2].x)
+            dx = walls[w1].x - walls[w2].x;
         else
-            dx = max(0, xyl[w2][0] - (xyl[w1][0] + xyl[w1][2]));
-        if (xyl[w2][1] >= xyl[w1][1])
-            dy = xyl[w2][1] - xyl[w1][1];
+            dx = max(0, walls[w2].x - (walls[w1].x + walls[w1].l));
+        if (walls[w2].y >= walls[w1].y)
+            dy = walls[w2].y - walls[w1].y;
         else
-            dy = max(0, xyl[w1][1] - (xyl[w2][1] + (-xyl[w2][2])));
+            dy = max(0, walls[w1].y - (walls[w2].y + (-walls[w2].l)));
     }
     return sqrt(dx*dx + dy*dy);
 }
 
 void process(int u){
-    for (int v = 0; v < n; ++v){
+    for (int v = 0; v < n; ++v)
         if (u != v && !vis[v] && g[u][v] < dsts[v]){
             dsts[v] = g[u][v];
             pathsize[v] = max(pathsize[u], dsts[v]);
         }
-    }
     vis[u] = 1;
 }
 
 void prim(){
     memset(pathsize, 0, sizeof(pathsize));
     memset(vis, 0, sizeof(vis));
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i <= n; ++i)
         dsts[i] = 1e9;
 
     process(0);
     for (int i = 0; i < n-1; ++i){
-        double mindst = 1e9, cur_ind = -1;
+        int cur_ind = n;
         for (int u = 0; u < n; ++u)
-            if (!vis[u] && dsts[u] < mindst)
-                {mindst = dsts[u]; cur_ind = u;}
+            if (!vis[u] && dsts[u] < dsts[cur_ind])
+                cur_ind = u;
         process(cur_ind);
     }
 }
@@ -81,7 +83,7 @@ int main(){
     scanf("%d", &n);
     while(n){
         for (int i = 0; i < n; ++i)
-            scanf("%d %d %d", &(xyl[i][0]), &(xyl[i][1]), &(xyl[i][2]));
+            scanf("%d %d %d", &(walls[i].x), &(walls[i].y), &(walls[i].l));
         
         for (int i = 0; i < n; ++i)
             for (int j = i+1; j < n; ++j)
