442aa6b3ce2acee973d70a1c74e2c7c4aeaa3055
Pietro Polinari Cavassin <pietro.polinari@gmail.com>
2022-06-17 17:50:56 -0300

    add min_queue structure

---
+++ utils/data_strucs/min_queue.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+/***** START MIN_QUEUE *****/
+
+deque<int> mq;
+void _remove(int elem){
+    if (!mq.empty() && mq.front() == elem) mq.pop_front();
+}
+
+void _push(int elem){
+    while (!mq.empty() && mq.back() > elem) mq.pop_back();
+    mq.push_back(elem);
+}
+
+int _front() {
+    return mq.front();
+}
+
+/***** END MIN_QUEUE *****/
