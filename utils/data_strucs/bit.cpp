74b1cc74b00fd868adfb06feca293dda150c58a6
Pietro Polinari Cavassin <pietro.polinari@gmail.com>
2022-07-11 15:22:13 -0300

    add bit data structure

---
+++ utils/data_strucs/bit.cpp
@@ -0,0 +1,15 @@
+/* ***** BEGIN BIT ***** */
+
+vector<int> bit (N, 0);
+void add(int i, int delta) {
+    for (; i < N; i += i & (-i))
+    bit[i] += delta;
+}
+int get(int i) {
+    int ans = 0;
+    for (; i > 0; i -= i & (-i))
+    ans += bit[i]s;
+    return ans;
+}
+
+/* ***** END BIT ***** */
