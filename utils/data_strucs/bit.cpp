c0f3112cb99c578a46e66ede45fdeb2e93fedf8a
Pietro Polinari Cavassin <pietro.polinari@gmail.com>
2022-07-14 21:09:15 -0300

    add bit

--- utils/data_strucs/bit.cpp
+++ utils/data_strucs/bit.cpp
@@ -1,11 +1,12 @@
 /***** BEGIN BIT *****/
+#define N_BIT 112345
 
-vector<int> bit (N, 0);
-void add(int i, int delta) {
-    for (; i < N; i += i & (-i))
+vector<int> bit (N_BIT, 0);
+void bit_add(int i, int delta) {
+    for (; i < N_BIT; i += i & (-i))
     bit[i] += delta;
 }
-int get(int i) {
+int bit_get(int i) {
     int ans = 0;
     for (; i > 0; i -= i & (-i))
     ans += bit[i];
