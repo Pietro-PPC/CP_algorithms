b385229babaffe3cba3b908ee5c2b93b9d525366
Pietro Polinari Cavassin <pietro.polinari@gmail.com>
2022-07-21 21:08:47 -0300

    update template

--- template.cpp
+++ template.cpp
@@ -5,12 +5,7 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-#define LL_INF 0x7f7f7f7f7f7f7f7f
-#define INT_INF 0x7f7f7f7f
 #define ende "\n"
-
-#define MP make_pair
-#define PB push_back
 
 int main(){
     ios::sync_with_stdio(0);
