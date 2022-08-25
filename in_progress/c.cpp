6237887320414ed9d3b125851b5efe7418b925b2
PIETRO POLINARI CAVASSIN <pietro.polinari@gmail.com>
2022-08-25 18:13:44 -0300

    [wip] week 11 c

--- in_progress/c.cpp
+++ in_progress/c.cpp
@@ -9,24 +9,37 @@ typedef __int128 i128;
 
 #define ende "\n"
 
-const int N = 8;
+#define FACTOR sz
+#define OP(X, Y) (X + Y)
+#define NEUTRAL 0
 
-vector<int> t (4*N); vector<bool> mark (4*N);
-void push(int ti) {
-    if (mark[ti]) {
-        t[ti*2] = t[ti*2+1] = t[ti];
-        t[ti] = 0;
-        mark[ti*2] = mark[ti*2+1] = true;
-        mark[ti] = false;
-    }
-}
+const int N = 2e5 + 15;
+// const int N = 8;
+
+vector<ll> t (4*N); vector<ll> lazy (4*N); vector<bool> mark (4*N, false);
 
 void push(int ti, int tl, int tm, int tr) {
-    t[ti*2] += lazy[ti] * FACTOR; 
+    { 
+        int sz = (tm - tl + 1);
+        if (mark[ti]){
+            t[ti*2] = t[ti];
+            mark[ti*2] = true;
+        }
+        t[ti*2] += lazy[ti] * FACTOR; 
+    }
     lazy[ti*2] += lazy[ti];
-    
-    t[ti*2+1] += lazy[ti] * FACTOR; 
+
+    { 
+        int sz = (tr - (tm+1) + 1);
+        if (mark[ti]){
+            t[ti*2+1] = t[ti];
+            mark[ti*2+1] = true;
+        }
+        t[ti*2+1] += lazy[ti] * FACTOR; 
+    }
     lazy[ti*2+1] += lazy[ti];
+
+    mark[ti] = false;
     lazy[ti] = 0;
 }
 
@@ -34,31 +47,44 @@ void add_inclusive(int l, int r, int d,
     int ti=1, int tl=1, int tr=N) {
     
     if (l > r) { return; }
-    if (l == tl && r == tr) { t[ti] += d; return; }
-    int tm = (tl + tr) / 2;
+    if (l == tl && tr == r) {
+        int sz = tr - tl + 1;
+        t[ti] += d * FACTOR; lazy[ti] += d; return; 
+    }
+
+    int tm = (tl + tr) / 2; push(ti, tl, tm, tr);
     add_inclusive(l, min(r, tm), d, ti*2, tl, tm);
     add_inclusive(max(l, tm+1), r, d, ti*2+1, tm+1, tr);
+    t[ti] = OP(t[ti*2], t[ti*2+1]);
 }
 
 void set_inclusive(int l, int r, int v,
     int ti=1, int tl=1, int tr=N) {
+    // return;
+
     if (l > r) { return; }
+    // if (l == tl && tr == r) {
+    //     t[ti] = v; mark[ti] = true; return; }
     if (l == tl && tr == r) {
-        t[ti] = v; mark[ti] = true; return; }
-    push(ti); int tm = (tl + tr) / 2;
+        int sz = tr - tl + 1;
+        t[ti] = v * FACTOR;
+        lazy[ti] = 0; 
+        mark[ti] = true;
+        return; 
+    }
+    int tm = (tl + tr) / 2; push(ti, tl, tm, tr);
     set_inclusive(l, min(r, tm), v, ti*2, tl, tm);
     set_inclusive(max(l, tm+1), r, v, ti*2+1, tm+1, tr);
 }
 
+ll op_inclusive(int l, int r,
+    int ti=1, int tl=1, int tr=N) {
 
-int get(int i, int ti=1, int tl=1, int tr=N) {
-    if (tl == tr) { return t[ti]; }
-    
-    int ret = mark[ti] ? 0 : t[ti];
-    push(ti); 
-    int tm = (tl + tr) / 2;
-    if (i <= tm) { return ret + get(i, ti*2, tl, tm); }
-    else { return ret + get(i, ti*2+1, tm+1, tr); }
+    if (l > r) { return NEUTRAL; }
+    if (l <= tl && tr <= r) { return t[ti]; }
+    int tm = (tl + tr) / 2; push(ti, tl, tm, tr);
+    return OP(op_inclusive(l, min(r, tm), ti*2, tl, tm),
+        op_inclusive(max(l, tm+1), r, ti*2+1, tm+1, tr));
 }
 
 void build(vector<int>& src,
@@ -67,10 +93,10 @@ void build(vector<int>& src,
         if (tl < src.size()) { t[ti] = src[tl]; }
         return;
     }
-    int tm = (tl+tr)/2;
+    int tm = (tl + tr) / 2;
     build(src, ti*2, tl, tm);
     build(src, ti*2+1, tm+1, tr);
-    t[ti] = 0;
+    t[ti] = OP(t[ti*2], t[ti*2+1]);
 }
 
 int main(){
@@ -92,12 +118,14 @@ int main(){
             int x; cin >> x;
             set_inclusive(a, b, x);
         } else {
-            int ans = 0;
-            while (a <= b)
-                {ans += get(a); a++;}
-            cout << ans << endl;
+            cout << op_inclusive(a, b) << endl;
         }
     }
+
+    // for (auto el : t){
+    //     cout << el << " ";
+    // }
+    // cout << ende;
 
     return 0;
 }
