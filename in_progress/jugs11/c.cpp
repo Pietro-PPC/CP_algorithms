a221dca77fcc0a70c5091706f10609ed0ab7e505
Pietro Polinari Cavassin <ppc19@inf.ufpr.br>
2022-08-26 21:42:21 -0300

    finish c

--- in_progress/jugs11/c.cpp
+++ in_progress/jugs11/c.cpp
@@ -13,87 +13,95 @@ typedef __int128 i128;
 #define OP(X, Y) (X + Y)
 #define NEUTRAL 0
 
-const int N = 2e5 + 15;
+const ll N = 2e5 + 15;
 // const int N = 8;
 
-vector<ll> t (4*N); vector<ll> lazy (4*N); vector<bool> mark (4*N, false);
+vector<ll> t (4*N); 
+vector<ll> lazyInc (4*N, 0);
+vector<ll> lazySet (4*N); 
+vector<bool> mark (4*N, false);
 
-void push(int ti, int tl, int tm, int tr) {
+void push(ll ti, ll tl, ll tm, ll tr) {
     { 
-        int sz = (tm - tl + 1);
+        ll sz = (tm - tl + 1);
         if (mark[ti]){
-            t[ti*2] = t[ti];
+            t[ti*2] = lazySet[ti] * FACTOR;
+            lazyInc[ti*2] = lazyInc[ti];
+            lazySet[ti*2] = lazySet[ti];
             mark[ti*2] = true;
         }
-        t[ti*2] += lazy[ti] * FACTOR; 
+        else lazyInc[ti*2] += lazyInc[ti];
+        t[ti*2] += lazyInc[ti] * FACTOR; 
     }
-    lazy[ti*2] += lazy[ti];
 
     { 
-        int sz = (tr - (tm+1) + 1);
+        ll sz = (tr - (tm+1) + 1);
         if (mark[ti]){
-            t[ti*2+1] = t[ti];
+            t[ti*2+1] = lazySet[ti] * FACTOR;
+            lazyInc[ti*2+1] = lazyInc[ti];
+            lazySet[ti*2+1] = lazySet[ti];
             mark[ti*2+1] = true;
         }
-        t[ti*2+1] += lazy[ti] * FACTOR; 
+        else lazyInc[ti*2+1] += lazyInc[ti];
+        t[ti*2+1] += lazyInc[ti] * FACTOR; 
     }
-    lazy[ti*2+1] += lazy[ti];
 
     mark[ti] = false;
-    lazy[ti] = 0;
+    lazyInc[ti] = 0;
 }
 
-void add_inclusive(int l, int r, int d,
-    int ti=1, int tl=1, int tr=N) {
+void add_inclusive(ll l, ll r, ll d,
+    ll ti=1, ll tl=1, ll tr=N) {
     
     if (l > r) { return; }
     if (l == tl && tr == r) {
-        int sz = tr - tl + 1;
-        t[ti] += d * FACTOR; lazy[ti] += d; return; 
+        ll sz = tr - tl + 1;
+        t[ti] += d * FACTOR; lazyInc[ti] += d; return; 
     }
 
-    int tm = (tl + tr) / 2; push(ti, tl, tm, tr);
+    ll tm = (tl + tr) / 2; push(ti, tl, tm, tr);
     add_inclusive(l, min(r, tm), d, ti*2, tl, tm);
     add_inclusive(max(l, tm+1), r, d, ti*2+1, tm+1, tr);
     t[ti] = OP(t[ti*2], t[ti*2+1]);
 }
 
-void set_inclusive(int l, int r, int v,
-    int ti=1, int tl=1, int tr=N) {
+void set_inclusive(ll l, ll r, ll v,
+    ll ti=1, ll tl=1, ll tr=N) {
     // return;
 
     if (l > r) { return; }
     // if (l == tl && tr == r) {
     //     t[ti] = v; mark[ti] = true; return; }
     if (l == tl && tr == r) {
-        int sz = tr - tl + 1;
+        ll sz = tr - tl + 1;
         t[ti] = v * FACTOR;
-        lazy[ti] = 0; 
+        lazySet[ti] = v; lazyInc[ti] = 0;
         mark[ti] = true;
         return; 
     }
-    int tm = (tl + tr) / 2; push(ti, tl, tm, tr);
+    ll tm = (tl + tr) / 2; push(ti, tl, tm, tr);
     set_inclusive(l, min(r, tm), v, ti*2, tl, tm);
     set_inclusive(max(l, tm+1), r, v, ti*2+1, tm+1, tr);
+    t[ti] = OP(t[ti*2], t[ti*2+1]);
 }
 
-ll op_inclusive(int l, int r,
-    int ti=1, int tl=1, int tr=N) {
+ll op_inclusive(ll l, ll r,
+    ll ti=1, ll tl=1, ll tr=N) {
 
     if (l > r) { return NEUTRAL; }
     if (l <= tl && tr <= r) { return t[ti]; }
-    int tm = (tl + tr) / 2; push(ti, tl, tm, tr);
+    ll tm = (tl + tr) / 2; push(ti, tl, tm, tr);
     return OP(op_inclusive(l, min(r, tm), ti*2, tl, tm),
         op_inclusive(max(l, tm+1), r, ti*2+1, tm+1, tr));
 }
 
-void build(vector<int>& src,
-    int ti=1, int tl=1, int tr=N) {
+void build(vector<ll>& src,
+    ll ti=1, ll tl=1, ll tr=N) {
     if (tl == tr) {
         if (tl < src.size()) { t[ti] = src[tl]; }
         return;
     }
-    int tm = (tl + tr) / 2;
+    ll tm = (tl + tr) / 2;
     build(src, ti*2, tl, tm);
     build(src, ti*2+1, tm+1, tr);
     t[ti] = OP(t[ti*2], t[ti*2+1]);
@@ -103,19 +111,19 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int n, q; cin >> n >> q;
-    vector<int> in(n+1);
-    for (int i = 1; i <= n; ++i) cin >> in[i];
+    ll n, q; cin >> n >> q;
+    vector<ll> in(n+1);
+    for (ll i = 1; i <= n; ++i) cin >> in[i];
     build(in);
 
-    for (int i = 0; i < q; ++i){
-        int op, a, b; 
+    for (ll i = 0; i < q; ++i){
+        ll op, a, b; 
         cin >> op >> a >> b;
         if (op == 1){
-            int x; cin >> x;
+            ll x; cin >> x;
             add_inclusive(a, b, x);
         } else if (op == 2){
-            int x; cin >> x;
+            ll x; cin >> x;
             set_inclusive(a, b, x);
         } else {
             cout << op_inclusive(a, b) << endl;
